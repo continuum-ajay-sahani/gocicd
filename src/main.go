cb85f9131dd857e667882619488a47611d678dcb
continuum-ajay-sahani <ajay.sahani@continuum.net>
2018-09-11 15:49:06 +0530

    added ping method

--- src/main.go
+++ src/main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("I am main function")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	r.Run(":8080")
 }
